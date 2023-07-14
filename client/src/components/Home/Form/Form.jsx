371d642556de4ed79724cfc00046d7f6540a86fa
Radu-A <elcastravete@gmail.com>
2023-07-14 19:30:17 +0200

    working/search_order

--- client/src/components/Home/Form/Form.jsx
+++ client/src/components/Home/Form/Form.jsx
@@ -1,46 +1,78 @@
 import React, { useEffect, useState } from "react";
+import { useForm } from 'react-hook-form';
 
+const Form = ({ projectList, updateProjectList }) => {
 
-function orderByDate(projectList) {
-  projectList.sort(function(a, b) {
-    var dateA = new Date(a.date);
-    var dateB = new Date(b.date);
-    return dateB - dateA;
-  });
-  return projectList;
-}
+  const [query, setQuery] = useState("");
 
-const Form = ({projectList, updateProjectList}) => {
+  const [keyword, setKeyword] = useState('');
+
 
   useEffect(() => {
-
-    const getProjects = async ()=> {
+    const getProjects = async () => {
       try {
-        const resp = await fetch('http://localhost:3000/api/projects');
+        const resp = await fetch("http://localhost:3000/api/projects");
         const data = await resp.json();
         updateProjectList(data);
       } catch (error) {
         console.log(error);
       }
-    }
+    };
     getProjects();
-  }, [])
+  }, []);
 
-  const handleSelectChanche = ()=> {
-    const orderedProjectList = orderByDate(projectList);
-    updateProjectList(orderedProjectList);
-    console.log(projectList);
-  }
+  useEffect(() => {
+    const getProjects = async () => {
+      try {
+        const resp = await fetch(`http://localhost:3000/api/projects/order?sort=${query}`);
+        const data = await resp.json();
+        updateProjectList(data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    getProjects();
+  }, [query]);
+
+  useEffect(() => {
+    const getProjects = async () => {
+      console.log('this is your keyword');
+      console.log(keyword);
+      try {
+        const resp = await fetch(`http://localhost:3000/api/projects/search?keyword=${keyword}`);
+        const data = await resp.json();
+        updateProjectList(data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    getProjects();
+  }, [keyword]);
+
+  const { register, handleSubmit, formState: { errors } } = useForm();
+  const onSubmit = data => {
+    console.log('this is your data');
+    console.log(data.Search);
+    setKeyword(data.Search);
+  };
+  console.log(errors);
+
+  const handleSelectChanche = (event) => {
+    setQuery(event.target.value);
+    console.log(query);
+  };
 
   return (
     <>
       <section className="form-section">
-        <input type="text" />
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <input type="text" placeholder="Search" {...register("Search", {})} />
+        </form>
         <select name="" id="" onChange={handleSelectChanche}>
           <option value="">order by</option>
-          <option value="">Curse</option>
-          <option value="">Date</option>
-          <option value="">User</option>
+          <option value="date">Date</option>
+          <option value="curse">Curse</option>
+          <option value="development">Development</option>
         </select>
       </section>
     </>
