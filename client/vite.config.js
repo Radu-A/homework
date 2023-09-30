683493cb11e9f0f5111743f02ca3db6d607eff04
Radu-A <elcastravete@gmail.com>
2023-09-30 15:49:10 +0200

    changed/vite_config

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,19 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      "/api": {
-        target:"https://homework-server-gzii.onrender.com",
-        changeOrigin: true,
-      },
-      "/auth": {
-        target:"https://homework-server-gzii.onrender.com",
-        changeOrigin: true,
-      },
-    }
-  }
-})
+});
